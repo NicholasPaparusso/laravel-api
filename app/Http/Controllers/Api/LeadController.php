7269ba031d24b692b354cf102877357a33b39844
NicholasPaparusso <nicopaparusso@gmail.com>
2023-02-01 08:47:41 +0100

    [add] validator message + loading

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,21 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use App\Mail\NewContact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request){
         $data = $request->all();
-
+        $success = true;
         $validator = Validator::make($data,
         [
             'name' => 'required|min:3|max:255',
             'object' => 'required|min:3|max:255',
             'email' => 'required|email|max:255',
             'message' => 'required|min:5'
-        ]);
+        ],
+        [
+            'name.required' => 'Il nome è un campo obbligatorio',
+            'name.min' => 'Il nome deve avere minimo :min caratteri',
+            'name.max' => 'Il nome deve avere massimo :max caratteri',
+            'object.required' => 'L\'oggetto è un campo obbligatorio',
+            'object.min' => 'L\'oggetto deve avere minimo :min caratteri',
+            'object.max' => 'L\'oggetto deve avere massimo :max caratteri',
+            'email.required' => 'L\'email è un campo obbligatorio',
+            'email.email' => 'Si prega d\'inserire un\'email valida',
+            'email.max' => 'L\'email deve avere massimo :max caratteri',
+            'message.required' => 'Il messaggio è un campo obbligatorio',
+            'message.min' => 'Il messaggio deve avere minimo :min caratteri',
+        ]
+    );
 
         if($validator->fails()){
             $success = false;
@@ -30,6 +47,9 @@ class LeadController extends Controller
         $new_lead->save();
 
         Mail::to('hello@example.com')->send(new NewContact($new_lead));
+
+       return response()->json(compact('success'));
+
 
     }
 }
--- resources/js/components/ContactForm.vue
+++ resources/js/components/ContactForm.vue
@@ -12,10 +12,12 @@ export default {
             email: '',
             message: '',
             errors: {},
+            isLoading: false,
         }
     },
     methods:{
         sendForm(){
+            this.isLoading= true;
             const data = {
                 name: this.name,
                 object: this.object,
@@ -24,8 +26,15 @@ export default {
             }
             axios.post(`http://127.0.0.1:8000/api/contacts`, data)
                 .then( result =>{
+                    this.isLoading = false;
                     if(!result.data.success){
                         this.errors = result.data.errors ;
+                    }else{
+                        this.name = '';
+                        this.object = '';
+                        this.email = '';
+                        this.message = '';
+                        this.errors = {};
                     }
                 })
         }
@@ -37,25 +46,25 @@ export default {
 
     <form class="w-50" @submit.prevent="sendForm()">
         <div class="mb-3">
-            <input v-model.trim="name" :class="{'is-invalid': errors.name}" class="form-control" type="text" name="name" placeholder="Inserisci il tuo nome">
+            <input v-model.trim="name" :class="{'is-invalid': errors.name}" class="form-control" type="text"  placeholder="Inserisci il tuo nome">
             <p v-for="(error , index) in errors.name " :key="error + index" class="text-danger ms-1"> {{ error}} </p>
         </div>
 
         <div class="mb-3">
-            <input v-model.trim="object" :class="{'is-invalid': errors.object}" class="form-control" type="text" name="object" placeholder="Inserisci l'oggetto">
+            <input v-model.trim="object" :class="{'is-invalid': errors.object}" class="form-control" type="text" placeholder="Inserisci l'oggetto">
             <p v-for="(error , index) in errors.object " :key="error + index" class="text-danger ms-1"> {{ error}} </p>
         </div>
 
         <div class="mb-3">
-            <input v-model.trim="email" :class="{'is-invalid': errors.email}" class="form-control" type="text" name="email" placeholder="Inserisci l'email">
+            <input v-model.trim="email" :class="{'is-invalid': errors.email}" class="form-control" type="text"  placeholder="Inserisci l'email">
             <p v-for="(error , index) in errors.email " :key="error + index" class="text-danger ms-1"> {{ error}} </p>
         </div>
 
         <div class="mb-3">
-            <textarea v-model.trim="message" :class="{'is-invalid': errors.message}" class="form-control" name="message" id="" cols="30" rows="10"></textarea>
+            <textarea v-model.trim="message" :class="{'is-invalid': errors.message}" class="form-control"  id="" cols="30" rows="10"></textarea>
             <p v-for="(error , index) in errors.message " :key="error + index" class="text-danger ms-1"> {{ error}} </p>
         </div>
-        <button type="submit" class="btn btn-success btn-sm">Invia </button>
+        <button type="submit" :disabled="isLoading" class="btn btn-success btn-sm">{{isLoading? 'invio in corso...' : 'Invia'}} </button>
     </form>
 
 </template>
